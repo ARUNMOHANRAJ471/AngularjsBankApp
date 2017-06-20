00a591f66b7b4ec6abe81ec490fafee6579b1082
ARUNMOHANRAJ471 <arunmohan471@gmail.com>
2017-06-20 16:26:52 +0530

    Routes added

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-var app = angular.module("myApp",[]);
+var app = angular.module("myApp",['ngRoute']);
+
+app.config(function($routeProvider){
+  $routeProvider
+  .when("/",{
+    templateUrl : "login.html",
+    controller: "loginController"
+  })
+  .when("/personal",{
+    templateUrl : "personal.html",
+    controller: "personalController"
+  })
+  .when("/account",{
+    templateUrl : "account.html",
+    controller: "accountController"
+  })
+  .when("/deposit",{
+    templateUrl : "deposit.html",
+    controller: "depositController"
+  })
+});
