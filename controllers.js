f3e257a8acf2d57c804d9a1710f3e8928eedbb68
ARUNMOHANRAJ471 <arunmohan471@gmail.com>
2017-06-20 12:20:29 +0530

    validation in login page completed

--- controllers.js
+++ controllers.js
@@ -2,6 +2,7 @@ app.controller("loginController",function($scope){
   console.log($scope);
     console.log($scope.emailData);
 
+  $scope.showWarning = { status: false};
   $scope.redirectToHome = function (){
     console.log("redirect function");
     console.log($scope.emailData +" ... "+$scope.passwordData);
@@ -9,6 +10,8 @@ app.controller("loginController",function($scope){
   if($scope.emailData == "arun" && $scope.passwordData == "123"){
     console.log("redirect if");
     window.location = "home.html";
+  }else{
+      $scope.showWarning.status = true;
   }
   }
 });
