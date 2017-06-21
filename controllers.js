23e46b7ed436464d7d77779aa0a6d2e8f921b72d
ARUNMOHANRAJ471 <arunmohan471@gmail.com>
2017-06-21 14:55:53 +0530

    personal page updated

--- controllers.js
+++ controllers.js
@@ -9,9 +9,20 @@ app.controller("loginController",function($scope){
 
   if($scope.emailData == "arun" && $scope.passwordData == "123"){
     console.log("redirect if");
-    window.location = "home.html";
+    window.location = "personal.html";
   }else{
       $scope.showWarning.status = true;
   }
   }
+});
+
+app.controller("personalController",function($scope){
+  $scope.personalData = [
+    {
+      name: "Arun Mohan Raj B",
+      address:"TamilNadu, India",
+      mail:"arun@example.com",
+      dob:"May 09, 1995",
+      image: "http://lh5.ggpht.com/_S0f-AWxKVdM/S5TpU6kRmUI/AAAAAAAAL4Y/wrjx3_23kw4/d_silhouette%5B2%5D.jpg?imgmax=800"
+    }]
 });
